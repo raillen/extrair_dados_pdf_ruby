43ed41cdff1a79e2ef5c3e728e6b09eb0f024e79
--global <raillendossantos@gmail.com>
2022-04-08 20:07:16 -0300

    Alterações

--- entidades/pdf.rb
+++ entidades/pdf.rb
@@ -1,46 +1,44 @@
 require 'pdf-reader'
 require 'json'
-require_relative 'utils.rb'
+require_relative 'utils'
 
 @no_pdf_related = "Hasn't pdf in this directory"
 
 def pdf_counter
-    pdf_arr = []
-    directory = Dir["#{'' + ARGV[0] + ''}/**/*.{pdf}"]
+  pdf_arr = []
+  directory = Dir["#{ARGV[0]}/**/*.{pdf}"]
 
-    # if there's pdf on folder, it'll push into array
-    directory.each { |pdf| pdf_arr << pdf} unless directory.empty?
+  # if there's pdf on folder, it'll push into array
+  directory.each { |pdf| pdf_arr << pdf } unless directory.empty?
 
-    pdf_arr
+  pdf_arr
 end
 
+def pdf_analysis(pdf_array)
+  data = []
 
-def pdf_analysis pdf_array
+  if pdf_array.any?
+    pdf_array.each do |pdf|
+      remove_dir = pdf.rindex('/') + 1
+      remove_extension = pdf.rindex('.')
 
-    data = []
-
-    unless pdf_array.empty?
-        pdf_array.each do |pdf|
-            remove_dir = pdf.rindex('/') + 1
-            remove_extension = pdf.rindex('.')
-
-            data.push({
-                pdf_name: pdf[remove_dir...remove_extension],
-                pages: PDF::Reader.new(pdf).page_count,
-                pdf_directory: pdf
-            })
-        end
-    else
-        data = @no_pdf_related
+      data.push(
+        {
+          pdf_name: pdf[remove_dir...remove_extension],
+          pages: PDF::Reader.new(pdf).page_count,
+          pdf_directory: pdf
+        })
     end
+  else
+    data = @no_pdf_related
+  end
 
-    data
+  data
 end
 
+def json_generator(data)
+  time = Time.now.strftime('%m-%d-%Y %k:%M:%S')
 
-def json_generator data
-    time = Time.now.strftime("%m-%d-%Y %k:%M:%S")
-
-    pdf_infos = OS.create_json_file("pdf_infos #{time}")
-    pdf_infos << JSON.pretty_generate(data)
+  pdf_infos = OS.create_json_file("pdf_infos #{time}")
+  pdf_infos << JSON.pretty_generate(data)
 end
--- entidades/utils.rb
+++ entidades/utils.rb
@@ -1,63 +1,52 @@
 require 'rbconfig'
 
+# módulos para auxiliarem na criação do arquivo e outras coisas corriqueiras como limpar o terminal.
 module OS
-    def self.name
-        RbConfig::CONFIG['host_os']
+  def self.name = RbConfig::CONFIG['host_os']
+  def self.clear = system('cls') || system('clear')
+
+  def self.create_json_file file_name = ''
+    home = ENV['HOME']
+    file = ''
+
+    case OS.name
+    when 'mingw32', 'mswin', 'darwin' # windows/mac
+      file = File.open(home + "/desktop/#{file_name}.json", 'w')
+    when 'linux'
+      file = File.open(home + "/#{file_name}.json", 'w')
+    else
+      file = File.open(home + "/#{file_name}.json", 'w')
     end
 
-    
-    def self.create_json_file file_name = ""
-        home = ENV['HOME']
-        file = ''
-    
-        case OS.name
-        when 'mingw32'#windows
-            file = File.open(home + "/desktop/#{file_name}.json", 'w')
-        when 'mswin' #windows
-            file = File.open(home + "/desktop/#{file_name}.json", 'w')
-        when 'linux'
-            file = File.open(home + "/#{file_name}.json", 'w')
-        when 'darwin'
-            file = File.open(home + "/desktop/#{file_name}.json", 'w')
-        else
-            file = File.open(home + "/#{file_name}.json", 'w')
-        end
-    
-        file
-    end
+    file
+  end
 
-    def self.install_dependencies
-        puts 'Instalando dependências...'
-        system("gem install bundler") if !system("gem list bundler")
-        system("bundle install") if !system('bundle check')
-        puts "Dependências instaladas."
-    end
+  def self.install_dependencies
+    puts 'Instalando dependências...'
+    system('gem install bundler') unless system('gem list bundler')
+    system('bundle install') unless system('bundle check')
+    puts 'Dependências instaladas.'
+  end
 
-    def self.clear
-        system('cls') || system('clear')
+  def self.json_ok
+    if OS.name == 'mingw32' || OS.name == 'darwin' || OS.name == 'mswin'
+      "\nArquivos .json gerado em #{ENV['HOME']}/desktop"
+    else
+      "\nArquivos .json gerado em #{ENV['HOME']}"
     end
+  end
 
-    def self.json_ok
-        if OS.name == 'mingw32' || OS.name == 'darwin' || OS.name == "mswin"
-            "\nArquivos .json gerado em #{ENV['HOME']}/desktop"
-        else
-            "\nArquivos .json gerado em #{ENV['HOME']}"
-        end
-    end
+  ## just a fake progress bar
+  def self.progress_bar(counter = [], sleep_time = 0)
+    arr_size = counter.size.to_f
 
-    ## just a fake progress bar
-    def self.progress_bar counter = [], sleep_time = 0
-        arr_size = counter.size.to_f
-        
-        
-        if counter.any?
-            0.step(100, (100/arr_size).ceil(2)) do |i|
-                print "Processando: #{i.ceil(2)}%\r"
-                sleep(sleep_time)
-            end
-        else
-            print "Processando: 100%"
-        end
+    if counter.any?
+      0.step(100, (100 / arr_size).ceil(2)) do |i|
+        print "Processando: #{i.ceil(2)}%\r"
+        sleep(sleep_time)
+      end
+    else
+      print 'Processando: 100%'
     end
+  end
 end
-
